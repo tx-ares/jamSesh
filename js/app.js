32ff98bf2e321c914eb6f82cba15de2007da4428
TxsAdamWest <Twinadanz@gmail.com>
2016-04-07 18:37:43 -0500

    improved splash and dash, made changes to splash

--- js/app.js
+++ js/app.js
@@ -1,49 +1,17 @@
 // es5, 6, and 7 polyfills, powered by babel
 import polyfill from "babel-polyfill"
-
-//
-// fetch method, returns es6 promises
-// if you uncomment 'universal-utils' below, you can comment out this line
 import fetch from "isomorphic-fetch"
-
-// universal utils: cache, fetch, store, resource, fetcher, router, vdom, etc
-// import * as u from 'universal-utils'
-
-// the following line, if uncommented, will enable browserify to push
-// a changed fn to you, with source maps (reverse map from compiled
-// code line # to source code line #), in realtime via websockets
-// -- browserify-hmr having install issues right now
-// if (module.hot) {
-//     module.hot.accept()
-//     module.hot.dispose(() => {
-//         app()
-//     })
-// }
-
-// Check for ServiceWorker support before trying to install it
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('./serviceworker.js').then(() => {
-//         // Registration was successful
-//         console.info('registration success')
-//     }).catch(() => {
-//         console.error('registration failed')
-//             // Registration failed
-//     })
-// } else {
-//     // No ServiceWorker Support
-// }
-
 import DOM from 'react-dom'
 import React, {Component} from 'react'
 import Backbone from 'bbfire'
 import Firebase from 'firebase'
 // window.BB = Backbone
 
-var ref = new Firebase("http://hushhush.firebaseio.com")
+var ref = new Firebase("http://jamcamp.firebaseio.com")
 
 var UserModel = Backbone.Firebase.Model.extend({
     initialize: function(uid) {
-        this.url = `http://hushhush.firebaseio.com/users/${uid}`
+        this.url = `http://jamcamp.firebaseio.com/users/${uid}`
     }
 })
 
@@ -57,7 +25,7 @@ var QueryByEmail = Backbone.Firebase.Collection.extend({
 // collection that will sync with a specific user's "messages" schema
 var UserMessages = Backbone.Firebase.Collection.extend({
     initialize: function(uid) {
-        this.url = `http://hushhush.firebaseio.com/users/${uid}/messages`
+        this.url = `http://jamcamp.firebaseio.com/users/${uid}/messages`
     }
 })
 
@@ -141,10 +109,24 @@ var Messenger = React.createClass({
 
     render: function() {
         return (
-            <div className="messager" >
-                <input placeholder="recipient email" onChange={this._setTargetEmail} />
-                <textarea placeholder="your message here" onChange={this._setMsg} />
-                <button onClick={this._submitMessage} >submit!</button>
+            <div className="dashContainer">
+                <div className="profile">
+                    <div className="imgContainer">
+                        <img className="profilePic" src="http://i.imgur.com/kQgQcDw.jpg?1"></img>
+                    </div>
+                    <ul>
+                        <li>Name</li>
+                        <li>Instruments</li>
+                        <li>Genre</li>
+                        <li><button>Create Band</button></li>
+                        <li>Delete Band</li>
+                    </ul>
+                </div>
+                <div className="messenger">
+                    <input placeholder="recipient email" onChange={this._setTargetEmail} />
+                    <textarea placeholder="your message here" onChange={this._setMsg} />
+                    <button onClick={this._submitMessage} >submit!</button> 
+                </div>
             </div>
             )
     }
@@ -166,24 +148,27 @@ var SplashPage = React.createClass({
     _updateEmail: function(event) {
         this.email = event.target.value
     },
-
-    _updateName: function(event) {
-        this.realName = event.target.value
-    },
-
+    // This function is currently disabled. //
+    // _updateName: function(event) {
+    //     this.realName = event.target.value
+    // },
+    //////////////////////////////////////////
     _updatePassword: function(event) {
         this.password = event.target.value
     },
 
     render: function() {
         return (
-            <div className="loginContainer">
-                <input onChange={this._updateEmail} />
-                <input onChange={this._updatePassword} type="password" />
-                <input placeholder="enter your real name" onChange={this._updateName} />
-                <div className="splashButtons" >
-                    <button onClick={this._handleSignUp} >sign up</button>
-                    <button onClick={this._handleLogIn} >log in</button>
+            <div className="splashContainer">
+                <img className="banner"></img>
+
+                <div className="loginContainer">
+                    <input onChange={this._updateEmail} />
+                    <input onChange={this._updatePassword} type="password" />
+                    <div className="splashButtons" >
+                        <button onClick={this._handleSignUp} >sign up</button>
+                        <button onClick={this._handleLogIn} >log in</button>
+                    </div>
                 </div>
             </div>
             )
@@ -201,7 +186,7 @@ function app() {
         },
 
         initialize: function() {
-            this.ref = new Firebase('https://hushhush.firebaseio.com/')
+            this.ref = new Firebase('https://jamcamp.firebaseio.com/')
             window.ref = this.ref
 
             if (!this.ref.getAuth()) {
